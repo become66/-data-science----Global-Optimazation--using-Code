17bedaf9668c2f25d0cff2ecb15f09bda9e39897
become66 <a0981695482@gmail.com>
2022-05-19 21:17:43 +0800

    fix the bug in the evalFunc function of DE_optimizer

--- 110065702_hw3.py
+++ 110065702_hw3.py
@@ -121,7 +121,7 @@ class DE_optimizer(Function): # need to inherit this class "Function"
         self.parameterPool = parameterPool
 
     def evalFunc(self, solution):
-        returnValue = self.f.evaluate(func_num, solution)
+        returnValue = self.f.evaluate(self.target_func, solution)
         if returnValue == "ReachFunctionLimit":
             print("ReachFunctionLimit")
             exit() 
